21eddf134835613ba543b0372ff016f426b04be9
Anthony Sellitti <anthony.sellitti@envato.com>
2015-02-15 21:33:59 +1100

    remove the cruft

--- www/php/include/twitter_user_timeline.php
+++
@@ -1,6 +0,0 @@
-		<div class="twitter-user-timeline">
-			<div class="main clear-fix">
-				<div class="twitter-user-timeline-list clear-fix"></div> 
-				<div class="twitter-user-timeline-background"></div>
-			</div>
-		</div>
