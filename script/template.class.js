11288d4e636c2f135f4af5352741284df79122b1
Anthony Sellitti <anthony.sellitti@envato.com>
2015-02-25 20:40:35 +1100

    what the fuck is this shit? seriously? what the fuck are they thinking?

--- script/template.class.js
+++ script/template.class.js
@@ -687,55 +687,6 @@ Template.prototype.createProgressBar=function(object,animate)
 
 /******************************************************************************/
 
-(function($) {
-	$.fn.countTo = function(options) {
-		// merge the default plugin settings with the custom options
-		options = $.extend({}, $.fn.countTo.defaults, options || {});
-
-		// how many times to update the value, and how much to increment the value on each update
-		var loops = Math.ceil(options.speed / options.refreshInterval),
-			increment = (options.to - options.from) / loops;
-
-		return $(this).each(function() {
-			var _this = this,
-				loopCount = 0,
-				value = options.from,
-				interval = setInterval(updateTimer, options.refreshInterval);
-
-			function updateTimer() {
-				value += increment;
-				loopCount++;
-				$(_this).html(value.toFixed(options.decimals));
-
-				if (typeof(options.onUpdate) == 'function') {
-					options.onUpdate.call(_this, value);
-				}
-
-				if (loopCount >= loops) {
-					clearInterval(interval);
-					value = options.to;
-
-					if (typeof(options.onComplete) == 'function') {
-						options.onComplete.call(_this, value);
-					}
-				}
-			}
-		});
-	};
-
-	$.fn.countTo.defaults = {
-		from: 0,  // the number the element should start at
-		to: 100,  // the number the element should end at
-		speed: 1000,  // how long it should take to count between the target numbers
-		refreshInterval: 100,  // how often the element should be updated
-		decimals: 0,  // the number of decimal places to show
-		onUpdate: null,  // callback method for every time the element is updated,
-		onComplete: null,  // callback method for when the element finishes updating
-	};
-})(jQuery);
-
-/******************************************************************************/
-
 Template.prototype.createCounter=function(object,animate)
 {
 	object=typeof(object)=='undefined' ? $('.counter') : object;
@@ -1085,12 +1036,7 @@ Template.prototype.createWaypoints=function()
 
 	$('.counter').waypoint(function()
 	{
-		$(this).countTo({
-			from: 50,
-			to: $(this).text(),
-			speed: 3000,
-			refreshInterval: 50
-		});
+		$this.createCounter($(this),true);
 	},
 	{
 		offset	:	'90%'
